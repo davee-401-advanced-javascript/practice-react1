20a511fdbed54b40388ff60ac4f62aecf31def88
Davee Sok <daveesok@gmail.com>
2020-11-14 20:33:16 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -3,30 +3,45 @@ import React from 'react';
 import './styles.scss';
 
 import Header from './header/header.js';
-
+import Form from './form/form.js';
+import Result from './result/result.js';
 import Footer from './footer/footer.js';
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-      this.state = {
-        submit: false,
-        headers: '',
-        count: 0,
-        results: []
-      };
-    }
-  
+	constructor(props) {
+		super(props);
+		this.state = {
+			submit: false,
+			headers: '',
+			count: 0,
+			results: [],
+		};
+	};
 
-  render() {
-    return (
-      <>
-      <Header/>
-      <Footer/>
-      </>
-    )
-  }
+	changeSubmit = () => {
+		this.setState({ submit: true });
+	}
+
+	update = (newHeaders, newCount, newResult) => {
+		this.setState({
+			headers: newHeaders, 
+			count: newCount,
+			results: newResult,
+		});
+	}
+
+
+	render() {
+		return (
+			<>
+				<Header />
+				<Form changeSubmit={this.changeSubmit} update={this.update}/>
+				{this.state.submit ? <Result submit={this.state.submit} headers={this.state.headers} count={this.state.count} results={this.state.results}/> : ''};
+				<Footer />
+			</>
+		);
+	}
 }
 
 export default App;
